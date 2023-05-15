fad60bc1f52a52344446e21a0e1a2729d966b864
Euan Goodbrand <euangoodbrand@aol.com>
2023-05-15 19:44:02 +0100

    Code to Connect to Wifi, interface and Run Motors for Card Shuffler
    Implemented motor control functions for card shuffler device
    
    This commit introduces four primary motor control functions for a card shuffler device:
    
    stop: Stops both motors.
    oppositeConstant: Runs both motors in opposite directions constantly.
    alternating: Alternates running each motor in opposite directions for a set duration.
    randomMotion: Randomly chooses a motor to run in a specific direction for a set duration.
    These functions are exposed as REST endpoints using the aREST library, which allows them to be triggered remotely. The device connects to a WiFi network, and the WiFiServer listens for incoming REST calls on a specific port. The device's ID and name are set using the aREST library. The IP address of the device is printed to the serial monitor for easy access.
    
    This update also includes setup for the Adafruit Motor Shield, and connection to the WiFi network.
    
    Please note: Replace the placeholder WiFi SSID and password with your actual WiFi credentials before using the code.

--- LA2/ProjectThing/ProjectThing.ino
+++ LA2/ProjectThing/ProjectThing.ino
@@ -1,3 +1,166 @@
-// ProjectThing.ino
+// Import required libraries
 
-// put your code here
+#include <WiFi.h>
+#include <aREST.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+ 
+// And connect 2 DC motors to port M3 & M4 !
+Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(4);
+Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(3);
+
+// Create aREST instance
+aREST rest = aREST();
+
+// WiFi parameters
+const char* ssid = "iPhone";
+const char* password = "password555";
+
+// The port to listen for incoming TCP connections 
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Function
+int stop(String message);
+int oppositeConstant(String message);
+int alternating(String message);
+int randomMotion(String message);
+
+
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(115200);
+
+  // Init motor shield
+  AFMS.begin();  
+
+  // Functions          
+  rest.function("stop", stop);
+  rest.function("oppositeConstant", oppositeConstant);
+  rest.function("alternating", alternating);
+  rest.function("randomMotion", randomMotion);
+      
+  // Give name and ID to device
+  rest.set_id("1");
+  rest.set_name("card_shuffler");
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+  
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+ 
+}
+
+int stop(String command) {
+  Serial.println("Stop");
+  // Stop
+  L_MOTOR->setSpeed(0);
+  L_MOTOR->run( RELEASE );
+ 
+  R_MOTOR->setSpeed(0);
+  R_MOTOR->run( RELEASE );
+
+  return 1;
+  
+}
+
+int oppositeConstant(String command) {
+  Serial.println("Constant");
+  // Run both motors in opposite directions constantly
+  L_MOTOR->setSpeed(200);
+  L_MOTOR->run(FORWARD);
+ 
+  R_MOTOR->setSpeed(200);
+  R_MOTOR->run(BACKWARD);
+
+  return 1;
+}
+
+int alternating(String command) {
+  // Run each motor one after the other in opposite directions on a loop for 1 second each
+  for (int i = 0; i < 5; i++) { // adjust the number of iterations as needed
+    Serial.println("Alternating");
+    L_MOTOR->setSpeed(200);
+    L_MOTOR->run(FORWARD);
+    R_MOTOR->setSpeed(0);
+    R_MOTOR->run(RELEASE);
+
+    delay(1000); // wait for 1 second
+
+    R_MOTOR->setSpeed(200);
+    R_MOTOR->run(BACKWARD);
+    L_MOTOR->setSpeed(0);
+    L_MOTOR->run(RELEASE);
+
+    delay(1000); // wait for 1 second
+  }
+  
+  // Make sure to stop the motors after the loop
+  L_MOTOR->setSpeed(0);
+  L_MOTOR->run(RELEASE);
+  R_MOTOR->setSpeed(0);
+  R_MOTOR->run(RELEASE);
+
+  return 1;
+}
+
+int randomMotion(String command) {
+  // Run each motor for one second randomly in opposite direction
+  for (int i = 0; i < 5; i++) { // adjust the number of iterations as needed
+    Serial.println("Random");
+    int randomMotor = random(0, 2); // generates either 0 or 1
+    
+    if (randomMotor == 0) {
+      L_MOTOR->setSpeed(200);
+      L_MOTOR->run(FORWARD);
+      R_MOTOR->setSpeed(0);
+      R_MOTOR->run(RELEASE);
+    } else {
+      R_MOTOR->setSpeed(200);
+      R_MOTOR->run(BACKWARD);
+      L_MOTOR->setSpeed(0);
+      L_MOTOR->run(RELEASE);
+    }
+
+    delay(1000); // wait for 1 second
+  }
+
+  // Make sure to stop the motors after the loop
+  L_MOTOR->setSpeed(0);
+  L_MOTOR->run(RELEASE);
+  R_MOTOR->setSpeed(0);
+  R_MOTOR->run(RELEASE);
+
+  return 1;
+}
